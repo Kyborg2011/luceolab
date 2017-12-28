9bd0718e5b8f9f8156b50e7b325ebdc0f545abdf
Anton Babinin <wkyborgw@gmail.com>
2017-12-28 05:35:12 +0200

    some changes [2]

--- www/server.js
+++ www/server.js
@@ -1,12 +1,12 @@
 let express = require( 'express' );
 let app = express();
 
-app.use( '/assets', express.static( 'dist/assets' ));
-app.use( '/css', express.static( 'dist/css' ));
-app.use( '/js', express.static( 'dist/js' ));
+app.use( '/assets', express.static( '../dist/assets' ));
+app.use( '/css', express.static( '../dist/css' ));
+app.use( '/js', express.static( '../dist/js' ));
 
 app.get( '/*', function( req, res ) {
-    res.sendFile( __dirname + '/dist/index.html' );
+    res.sendFile( __dirname + '../dist/index.html' );
 });
 
 var server = app.listen( 80, function() {
