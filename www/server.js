b96c6075856e17d3d775dd5f5e38a73b179a8107
Anton Babinin <wkyborgw@gmail.com>
2018-01-31 01:33:16 +0200

    some fixes of nodejs server

--- www/server.js
+++ www/server.js
@@ -1,8 +1,10 @@
 let path = require( 'path' );
 let express = require( 'express' );
 let compression = require( 'compression' );
+let serveStatic = require( 'serve-static' );
 
 let app = express();
+
 app.use( compression({
     filter: ( req, res ) => {
         if ( req.headers[ 'x-no-compression' ]) {
@@ -12,10 +14,9 @@ app.use( compression({
         return compression.filter( req, res );
     }
 }));
-
-app.use( '/assets', express.static( path.resolve( __dirname + './../dist/assets' )));
-app.use( '/css', express.static( path.resolve( __dirname + './../dist/css' )));
-app.use( '/js', express.static( path.resolve( __dirname + './../dist/js' )));
+app.use( '/assets', serveStatic( path.resolve( __dirname + './../dist/assets' )));
+app.use( '/css', serveStatic( path.resolve( __dirname + './../dist/css' )));
+app.use( '/js', serveStatic( path.resolve( __dirname + './../dist/js' )));
 
 app.get( '/*', function( req, res ) {
     res.sendFile( path.resolve( __dirname + './../dist/index.html' ));
