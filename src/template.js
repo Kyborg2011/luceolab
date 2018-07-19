d91ef395b8d9a8087b1d9cdb4349e61f8bbf5c0e
Anton Babinin <wkyborgw@gmail.com>
2018-07-19 04:31:37 +0300

    optimization fixes

--- src/template.js
+++ src/template.js
@@ -7,6 +7,20 @@ export default ({ body, helmet }) => {
             </head>
             <body>
                 <div id="root"><div>${body}</div></div>
+                <script type="text/javascript">
+                  WebFontConfig = {
+                      google: { families: [ 'Open+Sans+Condensed:300', 'Open+Sans:300,400,600', 'Josefin+Sans:300,600' ] }
+                  };
+                  (function() {
+                    var wf = document.createElement('script');
+                    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+                      '://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+                    wf.type = 'text/javascript';
+                    wf.async = 'true';
+                    var s = document.getElementsByTagName('script')[0];
+                    s.parentNode.insertBefore(wf, s);
+                  })();
+                </script>
                 <script src="/js/main.bundle.js"></script>
             </body>
         </html>
