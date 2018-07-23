68fff2c4aaf530c83c1f755b833368b90bd3a00d
Anton Babinin <wkyborgw@gmail.com>
2018-07-23 22:00:53 +0300

    + google analytics

--- src/components/content/pages/contact/Contact.js
+++ src/components/content/pages/contact/Contact.js
@@ -123,7 +123,7 @@ class Contact extends React.Component {
                             <p><span className="fa fa-envelope-o" /><a href="mailto:info@luceolab.com">info@luceolab.com</a></p>
                             <ul className={styles.socialNetworks}>
                                 <li className={styles.facebookBtn}>
-                                    <a href="#" target="_blank">
+                                    <a href="https://www.facebook.com/luceolabagency/" target="_blank">
                                         <svg viewBox="0 0 24 23"><path fill="inherit" d="M16.5,0 L7.4,0 C3.8,0 0.9,2.9 0.9,6.5 L0.9,15.6 C0.9,19.2 3.8,22.1 7.4,22.1 L16.5,22.1 C20.1,22.1 23,19.2 23,15.6 L23,6.5 C23,2.9 20.1,0 16.5,0 Z M21,15.5 C21,18 19,20 16.5,20 L14,20 L14,12 L17,12 L17,10 L14,10 L14,9 C14,7.9 14.9,7 16,7 L18,7 L18,5 L16,5 C13.8,5.1 12,6.8 12,9 L12,10 L10,10 L10,12 L12,12 L12,20 L7.5,20 C5,20 3,18 3,15.5 L3,6.4 C3,3.9 5,1.9 7.5,1.9 L16.6,1.9 C19.1,1.9 21.1,3.9 21.1,6.4 L21.1,15.5 L21,15.5 Z" /> </svg>
                                     </a>
                                 </li>
