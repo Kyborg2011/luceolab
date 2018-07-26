24e49890b9a76dca5fc24e067f74da9d900ea50e
Anton Babinin <wkyborgw@gmail.com>
2018-07-26 17:19:06 +0300

    add google analytics with react intergration

--- src/client.js
+++ src/client.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
 import App from './components/App';
+import GA from './components/shared/utils/GoogleAnalytics';
 
 ReactDOM.render((
      <BrowserRouter>
-          <Route path="/" component={App} />
+        { GA.init() && <GA.RouteTracker /> }
+        <Route path="/" component={App} />
      </BrowserRouter>
      ),
      document.getElementById( 'root' )
